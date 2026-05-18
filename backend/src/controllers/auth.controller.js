b1ca58b47913985a77a8f8a4a1f0d92e3beaab60
Subham777-max <subhamray865@gmail.com>
2026-05-18 22:37:45 +0530

    Implement user authentication controller and password hashing

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,54 @@
+import * as authService  from "../service/auth.service.js";
+import { setToken } from "../utils/setToken.js";
+import catchAsync from "../utils/catchAsync.js";
+
+const sendResponse = (res, statusCode, success, message, data) => {
+    res.status(statusCode).json({
+        success,
+        message,
+        data,
+    });
+}
+
+export const register = catchAsync(async (req, res) => {
+    const user = await authService.registerUser(req.body);
+    setToken(res, user);
+    sendResponse(res, 201, true, "User registered successfully", {
+        id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        role: user.role,
+    });
+});
+
+export const login = catchAsync(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await authService.loginUser(email, password);
+    setToken(res, user);
+    sendResponse(res, 200, true, "User logged in successfully", {
+        id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        role: user.role,
+    });
+});
+
+export const getMe = catchAsync(async (req, res) => {
+    const user = await authService.getMe(req.user.id);
+    sendResponse(res, 200, true, "User data retrieved successfully", {
+        id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        role: user.role,
+    });
+});
+
+export const logout = catchAsync(async (req, res) => {
+    res.clearCookie("token", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        maxAge: 0
+    });
+    sendResponse(res, 200, true, "User logged out successfully", null);
+});
